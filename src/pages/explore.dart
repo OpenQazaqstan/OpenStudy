9839d5cfa3dcf7b23fe8a23794aaa10482ce60c3
martiliones <martiliones@yandex.com>
2021-08-08 19:18:00 +0600

    Add icons

--- src/pages/explore.dart
+++ src/pages/explore.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_remix/flutter_remix.dart';
 
 import '../shared/colors.dart' as edu_colors;
 import '../shared/styles.dart' as styles;
@@ -20,12 +21,22 @@ class ExploreState extends State<Explore> {
       child: Padding(
         padding: EdgeInsets.only(left: 28, right: 28, top: 60),
         child: Column(children: <Widget>[
-          Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Все курсы',
-                style: styles.headerText,
-              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Все курсы',
+                    style: styles.headerText,
+                  )),
+              Icon(
+                FlutterRemix.search_line,
+                size: 20,
+                color: edu_colors.foreground,
+              ),
+            ],
+          )
         ]),
       ),
     ));
